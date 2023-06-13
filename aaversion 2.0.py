a9a31dc1978e7b7d55a2cabf9b1aa1b4b88f8cb5
icylapras24 <130667284+icylapras24@users.noreply.github.com>
2023-06-13 23:08:37 +0100

    Add files via upload
    added a feature to create a json file showing the average time over the date range with a bit stating the duration of the date range and other things

---
+++ aaversion 2.0.py
@@ -0,0 +1,103 @@
+# get the name of the filename of the application in use currently
+# record the time spent on an application by subtracting previous time from current time
+# 
+# convert it into a format suitable for json
+# write this time to a json file
+
+from time import sleep
+import datetime
+import json 
+from win32gui import GetForegroundWindow
+from win32process import GetWindowThreadProcessId
+import psutil
+
+# gets the name of the process ... (https://stackoverflow.com/questions/70574208/get-the-name-of-the-current-opened-application-in-python)
+def get_process_name():
+    hwnd = GetForegroundWindow()
+    _,pid = GetWindowThreadProcessId(hwnd)
+    if psutil.pid_exists(pid):
+        process = psutil.Process(pid)
+        process_name = process.name()
+        return process_name
+    else:
+        return "sleep"
+    
+# rounds times to nearest second
+def round_seconds(obj: datetime.datetime) -> datetime.datetime:
+    if obj.microsecond >= 500_000:
+        obj += datetime.timedelta(seconds=1)
+    return obj.replace(microsecond=0)
+
+if __name__ == '__main__':
+    print("---Timer for apps in Windows---")
+
+    #gets the data for python to read from the JSON
+    f =open("data_file.json")
+    string = f.read() 
+
+    timers_dict = json.loads(string)
+    print(timers_dict)
+    # resets the dictionary if wanted
+    yes_or_no = input("Do you want to reset the timers (y/n): ")
+    if yes_or_no == "y":
+        timers_dict = {}
+        print("yes"+"\n"+str(timers_dict))
+    elif yes_or_no == "n":
+        timers_dict = json.loads(string) # turns the json object format thing into a python dictionary
+        print("no")
+        for x, y in timers_dict.items():
+            print(x, ":", y)
+    else:
+        timers_dict = json.loads(string)
+        print("did not enter y/n, timers have not been reset")
+    f.close()
+
+    # setting up/initialising variables (after the input from the user so the time does not count the time spent answering the input question
+
+    old_time = round_seconds(datetime.datetime.now())
+    active_window = get_process_name()
+    new_window = ""
+    count = 0
+    statement_executed = False
+    start_time = round_seconds(datetime.datetime.now())
+
+    print(start_time)
+    while True:
+        new_window = get_process_name()
+        if (new_window != active_window) or (count>=30):
+            current_time = round_seconds(datetime.datetime.now()) #current datetime = the datetime now and rounds to nearest second
+            
+            if active_window not in timers_dict:
+                timers_dict.update({active_window: "0:00:00"})
+            
+            # getting the time from the dictionary and converting to timedelta
+            total_time = timers_dict[active_window]
+            total_time = datetime.datetime.strptime(total_time,"%H:%M:%S") # we specify the input and the format...
+            total_time = datetime.timedelta(hours=total_time.hour, minutes=total_time.minute, seconds=total_time.second) # ...and use datetime's hour, min and sec properties to build a timedelta
+            
+            total_time += current_time - old_time # total time for the thing
+            old_time = current_time
+        
+            str_total_time = str(total_time)
+            timers_dict.update({active_window: str_total_time}) # updates the dictionary with the current time if the dictionary does not have that window stored
+            jsonObject = json.dumps(timers_dict, indent=4) # converts timers_dict into JSON string
+            
+            with open("data_file.json", "w") as write_file:
+                        write_file.write(jsonObject) # writes to JSON file
+            
+            active_window = new_window
+            statement_executed = True
+
+        if (statement_executed == True): #if the if statement before was executed or it has been 300 seconds (5 minutes) then prints the times along with the curren time
+            print("Time :", datetime.datetime.now().strftime("%H:%M:%S")) #current time in hrs mins and secs
+            #prints the dictionary line by line
+            for apps,times in timers_dict.items():
+                print(apps, ":", times) 
+            print("")
+            statement_executed = False
+            count = 0
+            if current_time-start_time >= datetime.timedelta(minutes=30): # displays this message every 30 minutes cuz why not
+                print("\n"+"---30 MINUTES---"+"\n")
+                start_time = current_time
+        count +=1
+        sleep(10)
--- storetimes.py
+++ storetimes.py
@@ -2,9 +2,15 @@
 
 import json
 import datetime
+import math 
 
 def str_to_timedelta(str_of_timedelta): # input the value of the key and get out a timedelta
     converted_str = datetime.datetime.strptime(str_of_timedelta,"%H:%M:%S") # we specify the input and the format...
+    converted_str = datetime.timedelta(hours=converted_str.hour, minutes=converted_str.minute, seconds=converted_str.second) # ...and use datetime's hour, min and sec properties to build a timedelta
+    return converted_str
+
+def special_str_to_timedelta(str_of_timedelta): # input the value of the key and get out a timedelta
+    converted_str = datetime.datetime.strptime(str_of_timedelta,"%H:%M:%S.%f") # we specify the input and the format...
     converted_str = datetime.timedelta(hours=converted_str.hour, minutes=converted_str.minute, seconds=converted_str.second) # ...and use datetime's hour, min and sec properties to build a timedelta
     return converted_str
 
@@ -14,44 +20,60 @@ def add_to_total(old_time_val): #gets the string from both dictionaries, convert
     total_time = new_time + old_time
     return total_time
 
+# rounds times to nearest second
+def round_seconds(obj: datetime.datetime) -> datetime.datetime:
+    if obj.microsecond >= 500_000:
+        obj += datetime.timedelta(seconds=1)
+    return obj.replace(microsecond=0)
 
-#retrieves object from data_file.json and converts to python dict
-f = open("data_file.json")
-json_new_dict = f.read() 
-f.close()
-new_dict = json.loads(json_new_dict)
+def special_round_seconds(timedelta):
+    timedelta_seconds = datetime.timedelta.total_seconds(timedelta)
+    timedelta_full = datetime.timedelta(seconds=int(timedelta_seconds))
+    return timedelta_full
 
-#retrieves object big_log.json and converts to python dict
-f = open("big_log.json")
-json_big_log = f.read() 
-f.close()
-big_log = json.loads(json_big_log)
+if __name__ == '__main__':
 
-# stores current_date as current date and sets up date_is_today bool
-current_date = str(datetime.date.today())
-date_is_today = False
+    #retrieves object from data_file.json and converts to python dict
+    f = open("data_file.json")
+    json_new_dict = f.read() 
+    f.close()
+    new_dict = json.loads(json_new_dict)
 
-# if the current date is not in big_log then do this, otherwise do that
-for date in big_log: # checks every "date" in the dictionary big_log
-    if date == current_date: # if it matches the current date
-        date_is_today = True 
-        for key, value in new_dict.items(): # idk why items but idc
-            if key in big_log[date]: # if the app is already in the log for that day then add to the value
-                total_time = add_to_total(big_log[date][key])
-                big_log[date].update({key: str(total_time)})
-            else: 
-                big_log[date][key] = new_dict[key]
-if date_is_today != True:
-    dict(big_log).update({current_date: new_dict})
+    #retrieves object big_log.json and converts to python dict
+    f = open("big_log.json")
+    json_big_log = f.read() 
+    f.close()
+    big_log = json.loads(json_big_log)
 
-for i in dict(big_log).items():
-    print(i)
+    # stores current_date as current date and sets up date_is_today bool
+    current_date = str(datetime.date.today())
+    date_is_today = False
+    
+    # if the current date is not in big_log then do this, otherwise do that
+    for date in big_log: # checks every "date" in the dictionary big_log
+        if date == current_date: # if it matches the current date
+            date_is_today = True
+            print("date is in database already") 
+            for key, value in new_dict.items(): 
+                if key in big_log[date]: # if the app is already in the log for that day then add to the value
+                    total_time = add_to_total(big_log[date][key])
+                    big_log[date].update({key: str(total_time)})
+                    print("program already in database, added to database")
+                else: 
+                    big_log[date][key] = new_dict[key]
+                    print("program not in database, making new dictionary for it")
+    if date_is_today != True:
+        dict(big_log).update({current_date: new_dict})
+        print("date is not in database")
+    print(big_log)
+    # adds times from all dates if wanted by the user
 
-# adds times from all dates if wanted by the user
-choice = input("do you want to see the total use time of all applications in the big log (y/n)")
-if choice == "y":
     total_dict = {} # initialises the dict for this task
+    print("initailised total_dict")
     for date in big_log: # iterates through all the dates
+        
+        print("going through dates")
+        
         for key, value in big_log[date].items(): # gets all the keys and values from a date
             
             if key in total_dict: # if a key from this date is already in the total dict
@@ -63,33 +85,64 @@ if choice == "y":
     
     # turns the dictionary into an array
     total_list = list(map(list, total_dict.items()))
-    print(total_list)
 
-    for i in range(len(total_list)):
-        total_list[i][1] = str_to_timedelta(total_list[i][1])
-        print(total_list)
-    
+    no_of_dates = len(big_log)
+
+    for i in range(len(total_list)): # converts the array into timedelta and at the same time divides it by the number of dates in big log
+        total_list[i][1] = str_to_timedelta(total_list[i][1]) / no_of_dates
+        
     # sort the list
     total_list.sort(key=lambda total_list:total_list[1], reverse=True)
-    print(total_list)
+    
+    deletion_complete = False
+    if len(total_list) > 5: # so it doesnt crash when theres only a few values    
+        for i in range(len(total_list)):
+            while deletion_complete == False:
+                if total_list[-i - 1][1] > datetime.timedelta(minutes=2):
+                    deletion_complete = True
+                elif total_list[-i - 1][1] < datetime.timedelta(minutes=2):
+                    del total_list[-i - 1]
+
+    # round the numbers before printing out
+        for i in range(len(total_list)):
+            total_list[i][1] = special_round_seconds(total_list[i][1])
+
 
     # prints the total dict out
     for rows, columns in total_list:
         print(rows, ":", str(columns))
-else:
-    print("no")
+        
+    # writing the time for today to big log
+    json_object = json.dumps(big_log, indent=4) # converts timers_dict into JSON string
+        
+    with open("big_log.json", "w") as write_file:
+        write_file.write(json_object) # writes to JSON file
 
+    # restarting the new log and writing the empty new log to data_file.json
+    new_log = {}
+    json_object = json.dumps(new_log, indent=4)
+    with open("data_file.json", "w") as clear_file:
+        clear_file.write(json_object)
 
-jsonObject = json.dumps(big_log, indent=4) # converts timers_dict into JSON string
+    for i in range(len(total_list)):
+        total_list[i][1] = str(total_list[i][1])
+
+    list_of_dates = [] # for later when calculating the date range of the data
+    for date in big_log:      
+        list_of_dates.append(datetime.date.fromisoformat(date)) # creating a list with all the dates 
+
+    sorted_list_of_dates = sorted(list_of_dates) # sorts the list of dates in ascending order 
+    date_range = sorted_list_of_dates[-1] - sorted_list_of_dates[1] # takes away the earliest date in the list from the latest date to get a length of time
+    string_range_of_days = str(date_range.days) + " days"
+
+    range_dict = {"RANGE OF DAYS TO CALCULATE AVERAGE" : string_range_of_days}
+    average_times = dict(total_list)
+    average_times.update(range_dict)
+    json_object = json.dumps(average_times, indent=4)
+    json_range_of_days_dict = json.dumps(range_dict, indent=4)
+    with open("average_times.json", "w") as f:
+        f.write(json_object)
+
+    print("RANGE OF DAYS TO CALUCLATE AVERAGE : " + string_range_of_days)
+    # make it so the output is in descending order from longest to shortest times
     
-with open("big_log.json", "w") as write_file:
-    write_file.write(jsonObject) # writes to JSON file
-
-
-new_log = {}
-jsonObject = json.dumps(new_log, indent=4)
-with open("data_file.json", "w") as clear_file:
-    clear_file.write(jsonObject)
-
-
-# make it so the output is in descending order from longest to shortest times
